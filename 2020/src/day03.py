b5d2e210991a35460cb71e34f0cbfc07ee5851c8
AlcatrazEscapee <alex@molleroneill.com>
2020-12-03 00:29:52 -0500

    Day 3, minified!

--- 2020/src/day03.py
+++ 2020/src/day03.py
@@ -10,13 +10,8 @@ def main(lines: List[str]):
     print('Part 2:', prod(count_trees_hit(lines, x, y) for x, y in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))))
 
 
-def count_trees_hit(lines: List[str], step_x: int, step_y: int):
-    trees = 0
-    for i in range(0, len(lines) // step_y):
-        line = lines[i * step_y]
-        if line[(i * step_x) % len(line)] == '#':
-            trees += 1
-    return trees
+def count_trees_hit(lines: List[str], x: int, y: int):
+    return sum(lines[i * y][(i * x) % len(lines[i * y])] == '#' for i in range(0, len(lines) // y))
 
 
 if __name__ == '__main__':
