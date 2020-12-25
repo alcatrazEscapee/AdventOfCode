8658fc5d8edfc56df1d9b8c342766192ab966362
AlcatrazEscapee <alex@molleroneill.com>
2020-12-25 02:23:10 -0500

    Switched back to original day 8 solution as the assembly never got reused.

--- 2020/src/day08.py
+++ 2020/src/day08.py
@@ -4,28 +4,41 @@
 from utils import *
 
 
-def main(lines: List[str]):
-    code = Asm.parse(lines)
-    print('Part 1:', halting_problem(code).accumulator)
-    for i in range(len(code)):
+def main():
+    code = []
+    for line in get_input_lines():
+        parts = line.split(' ')
+        code.append((parts[0], int(parts[1])))
+
+    print('Part 1:', halting_problem(code)[1])
+
+    for i, spot in enumerate(code):
         code_test = code[:]
-        inst = code_test[i]
-        if inst[0] in (Opcode.nop, Opcode.jmp):
-            code_test[i] = (Opcode.nop if inst[0] == Opcode.jmp else Opcode.jmp, *inst[1:])
-            r = halting_problem(code_test)
-            if not r.running:
-                print('Part 2:', r.accumulator)
+        if code_test[i][0] == 'nop':
+            code_test[i] = ('jmp', code_test[i][1])
+        elif code_test[i][0] == 'jmp':
+            code_test[i] = ('nop', code_test[i][1])
+
+        r, v = halting_problem(code_test)
+        if not r:
+            print('Part 2:', v)
 
 
-def halting_problem(code: List[Tuple[int, ...]]) -> Asm:
-    runner = Asm(code)
-    runner.running = True
-    hits = set()
-    while runner.running and runner.pointer not in hits:
-        hits.add(runner.pointer)
-        runner.tick()
-    return runner
+def halting_problem(code: List[Tuple[str, int]]) -> Tuple[bool, int]:
+    pointer = 0
+    acc = 0
+    ran = set()
+    while 0 <= pointer < len(code) and pointer not in ran:
+        ran.add(pointer)
+        if code[pointer][0] == 'acc':
+            acc += code[pointer][1]
+            pointer += 1
+        elif code[pointer][0] == 'jmp':
+            pointer += code[pointer][1]
+        elif code[pointer][0] == 'nop':
+            pointer += 1
+    return pointer in ran, acc
 
 
 if __name__ == '__main__':
-    main(get_input_lines())
+    main()
