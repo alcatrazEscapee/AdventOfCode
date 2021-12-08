35abba14070fa9374da377cf0458c4a401b35a86
AlcatrazEscapee <alex@molleroneill.com>
2021-12-08 00:58:56 -0500

    Day 8: Lots of fun! Implemented a proper deductive solution rather than a brute force one, which cost time, but it was a really interesting problem.

--- 2021/src/day07.py
+++ 2021/src/day07.py
@@ -1,10 +1,12 @@
+# Day 7: The Treachery of Whales
+
 from utils import get_input, ints
 from typing import Tuple, Callable
 
 
 def main():
     values = ints(get_input())
-    print('Part 1:',fuel(values, lambda x: x))
+    print('Part 1:', fuel(values, lambda x: x))
     print('Part 2:', fuel(values, lambda x: (x + 1) * x // 2))
 
 def fuel(values: Tuple[int], cost: Callable[[int], int]) -> int:
