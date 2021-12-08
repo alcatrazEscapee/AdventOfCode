8c730068d4df23eaea82448fc15f4b82d6e48833
AlcatrazEscapee <alex@molleroneill.com>
2021-12-08 11:09:13 -0500

    Fix day 7 part 2

--- 2021/src/day07.py
+++ 2021/src/day07.py
@@ -10,7 +10,7 @@ def main():
     print('Part 2:', fuel(values, lambda x: (x + 1) * x // 2))
 
 def fuel(values: Tuple[int], cost: Callable[[int], int]) -> int:
-    return min(sum(cost(abs(p - c)) for c in values) for p in values)
+    return min(sum(cost(abs(p - c)) for c in values) for p in range(min(values), 1 + max(values)))
 
 if __name__ == '__main__':
     main()
