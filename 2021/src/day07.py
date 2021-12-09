c24215356ea97b3a3df049dc1f188332af4212b5
AlcatrazEscapee <alex@molleroneill.com>
2021-12-09 00:20:59 -0500

    Improve analytical solution for day 7: reduce runtime to from ~500ms to ~100ms by taking advantage of some calculus.

--- 2021/src/day07.py
+++ 2021/src/day07.py
@@ -6,11 +6,20 @@ from typing import Tuple, Callable
 
 def main():
     values = ints(get_input())
-    print('Part 1:', fuel(values, lambda x: x))
-    print('Part 2:', fuel(values, lambda x: (x + 1) * x // 2))
 
-def fuel(values: Tuple[int], cost: Callable[[int], int]) -> int:
-    return min(sum(cost(abs(p - c)) for c in values) for p in range(min(values), 1 + max(values)))
+    # Analytical improvements:
+    # For part 1, we can show the target location will lie in an interval where there are equal number of points on either side (so +1/-1 of the median)
+    # For part 2, we can show the target location will lie within +/- 1 of the mean.
+    # This improves the overall runtime by about 10x as opposed to checking every location
+
+    ordered = sorted(values)
+    midpoint = len(values) // 2
+    mean = sum(values) // len(values)
+    print('Part 1:', fuel(values, lambda x: x, ordered[midpoint - 1], ordered[midpoint + 1]))
+    print('Part 2:', fuel(values, lambda x: (x + 1) * x // 2, mean - 1, mean + 1))
+
+def fuel(values: Tuple[int], cost: Callable[[int], int], lo: int, hi: int) -> int:
+    return min(sum(cost(abs(p - c)) for c in values) for p in range(lo, 1 + hi))
 
 if __name__ == '__main__':
     main()
