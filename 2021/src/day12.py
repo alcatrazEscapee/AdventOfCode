3caab097a4a316c5beb2f009966fe95361150296
AlcatrazEscapee <alex@molleroneill.com>
2021-12-12 13:37:41 -0500

    Cleaner implementation of the graph traversal for day 12: use a single 'has visited double state' variable to track part 2 paths, and infer both answers from part 2 via filtering.

--- 2021/src/day12.py
+++ 2021/src/day12.py
@@ -1,55 +1,45 @@
 from utils import get_input
 from collections import defaultdict
-from typing import Set, List, Tuple, Optional, DefaultDict
+from typing import Set, List, Tuple, DefaultDict
 
 
 def main(text: str):
 
     # Assemble the graph
-    edges: DefaultDict[str, Set[str]] = defaultdict(set)
+    graph: DefaultDict[str, Set[str]] = defaultdict(set)
     for line in text.split('\n'):
         s, e = line.split('-')
-        edges[s].add(e)
-        edges[e].add(s)
+        graph[s].add(e)
+        graph[e].add(s)
 
     # Remove edges from 'end' -> X and X -> 'start', to give nice termination conditions
-    edges['end'] = set()
-    for k, v in edges.items():
+    graph['end'] = set()
+    for k, v in graph.items():
         v -= {'start'}
 
-    print('Part 1:', total_paths(edges, False))
-    print('Part 2:', total_paths(edges, True))
-
-
-def total_paths(graph: DefaultDict[str, Set[str]], allow_double_small_node_traversal: bool) -> int:
-    """ Number of paths given that:
-    - Large nodes can be encountered any number of times
-    - Small nodes can be encountered once (or if allow_double_small_node_traversal, then a single node may be encountered twice)
-    In order to track this, we actually track the sum of all paths that have a *specific* small node that may be encountered twice
-    """
-    paths = set()
-    queue: List[Tuple[Tuple[str, ...], Set[str], Optional[str]]]
-
-    if allow_double_small_node_traversal:
-        queue = [(('start',), set(), n) for n in graph.keys() if n.islower()]
-    else:
-        queue = [(('start',), set(), None)]  # This path artificially has no small nodes which it can traverse twice
+    part1 = set()
+    part2 = set()
+    queue: List[Tuple[Tuple[str, ...], Set[str], bool]] = [(('start',), set(), True)]
 
     while queue:
-        path, seen, small = queue.pop()
+        path, seen, repeat = queue.pop()
         node = path[-1]
 
         if node == 'end':
-            paths.add(path)
+            part2.add(path)
+            if repeat:  # If the repeated node is still available, this path qualifies for part1 counting
+                part1.add(path)
 
         for step in graph[node]:
             if step.isupper():  # Allowed to traverse large nodes any number of times
-                queue.append(((*path, step), set(seen), small))
+                queue.append(((*path, step), seen, repeat))
             elif step not in seen:  # Small nodes must be encountered once (in general)
-                queue.append(((*path, step), seen | {step}, small))
-            elif step == small:  # Except for a *specific* small node to this path, which can be encountered twice
-                queue.append(((*path, step), set(seen), None))
-    return len(paths)
+                queue.append(((*path, step), seen | {step}, repeat))
+            elif repeat:  # Except for a single small node to this path, which can be encountered twice
+                queue.append(((*path, step), seen, False))
+
+    print('Part 1:', len(part1))
+    print('Part 2:', len(part2))
 
 
 if __name__ == '__main__':
