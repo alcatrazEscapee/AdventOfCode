d62e85e9d6a612253f9ff01728eab2151f00d220
AlcatrazEscapee <alex@molleroneill.com>
2021-12-14 01:00:39 -0500

    Refactor day 1 to use `windows` as it has been added to utils.

--- 2021/src/day01.py
+++ 2021/src/day01.py
@@ -1,13 +1,13 @@
 # Day 01: Sonar Sweep
 
-from utils import get_input, ints
+from utils import get_input, ints, windows
 
 
 def main(text: str):
     values = ints(text)
 
-    print('Part 1:', sum(values[i] < values[i + 1] for i in range(len(values) - 1)))
-    print('Part 2:', sum(values[i] < values[i + 3] for i in range(len(values) - 3)))
+    print('Part 1:', sum(a < b for a, b in windows(values, 2)))
+    print('Part 2:', sum(a < b for a, _, _, b in windows(values, 4)))
 
 
 if __name__ == '__main__':
