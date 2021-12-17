282be21e3a8e32419d156bee92b7c00ffe901f3d
AlcatrazEscapee <alex@molleroneill.com>
2021-12-17 18:10:55 -0500

    Day 17, runtime improvements, going from O(n^3) -> O(n^2), ish. Runtime is acceptable up to target areas ~100x the original puzzle size.

--- 2021/src/day17.py
+++ 2021/src/day17.py
@@ -1,29 +1,43 @@
 from utils import get_input, ints
+
 from math import sqrt, floor, ceil
+from collections import defaultdict
 
 
 def main(text: str):
     min_x, max_x, min_y, max_y = ints(text)
 
-    velocities = set()
+    y_velocities = set()
+    n2v = defaultdict(set)
+
     for yt in range(min_y, 1 + max_y):
         for n in range(1, 1 + 2 * abs(yt)):
             if (2 * yt) % n == 0 and (v0 := 2 * yt - n + n * n) % (2 * n) == 0:
                 v = v0 // (2 * n)
-                for xt in range(min_x, 1 + max_x):
-                    # Case: u <= n
-                    k = sqrt(2 * xt)
-                    for u in range(floor(k) - 1, 1 + ceil(k)):
-                        if u <= n and u * (u + 1) == 2 * xt:
-                            velocities.add((u, v))
-                    # Case: u > n
-                    if (2 * xt) % n == 0 and (u0 := 2 * xt - n + n * n) % (2 * n) == 0:
-                        u = u0 // (2 * n)
-                        if u > n:
-                            velocities.add((u, v))
+                n2v[n].add(v)
+                y_velocities.add(v)
 
-    print('Part 1:', max(v * (v + 1) // 2 for _, v in velocities))
-    print('Part 2:', len(velocities))
+    print('Part 1:', max(v * (v + 1) // 2 for v in y_velocities))
+
+    trajectories = set()
+    n2v_seq = sorted(n2v.items(), key=lambda x: -x[0])
+    for xt in range(min_x, 1 + max_x):
+        k = sqrt(2 * xt)
+        for u in range(floor(k) - 1, 1 + ceil(k)):
+            if u * (u + 1) == 2 * xt:
+                for n, vs in n2v_seq:
+                    if u > n:
+                        break
+                    for v in vs:
+                        trajectories.add((u, v))
+        for n, vs in n2v_seq:
+            if (2 * xt) % n == 0 and (u0 := 2 * xt - n + n * n) % (2 * n) == 0:
+                u = u0 // (2 * n)
+                if u > n:
+                    for v in vs:
+                        trajectories.add((u, v))
+
+    print('Part 2:', len(trajectories))
 
 
 if __name__ == '__main__':
