52034768c71408e60d75fef106f6f65ea94476d5
AlcatrazEscapee <alex@molleroneill.com>
2021-12-17 12:55:31 -0500

    Re-order loops for improved efficiency, and implement the sqrt(2x_T) bound for checking u values.

--- 2021/src/day17.py
+++ 2021/src/day17.py
@@ -1,20 +1,21 @@
 from utils import get_input, ints
+from math import sqrt, floor, ceil
 
 
 def main(text: str):
     min_x, max_x, min_y, max_y = ints(text)
 
     velocities = set()
-    for xt in range(min_x, 1 + max_x):
-        for yt in range(min_y, 1 + max_y):
-            for n in range(1, 1 + 2 * abs(yt)):
-                if (2 * yt) % n == 0 and (v0 := 2 * yt - n + n * n) % (2 * n) == 0:
-                    v = v0 // (2 * n)
+    for yt in range(min_y, 1 + max_y):
+        for n in range(1, 1 + 2 * abs(yt)):
+            if (2 * yt) % n == 0 and (v0 := 2 * yt - n + n * n) % (2 * n) == 0:
+                v = v0 // (2 * n)
+                for xt in range(min_x, 1 + max_x):
                     # Case: u <= n
-                    for u in range(1, 1 + min(n, abs(xt))):
-                        if u * (u + 1) == 2 * xt:
+                    k = sqrt(2 * xt)
+                    for u in range(floor(k), 1 + ceil(k)):
+                        if u <= n and u * (u + 1) == 2 * xt:
                             velocities.add((u, v))
-                            break
                     # Case: u > n
                     if (2 * xt) % n == 0 and (u0 := 2 * xt - n + n * n) % (2 * n) == 0:
                         u = u0 // (2 * n)
