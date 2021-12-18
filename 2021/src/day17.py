605785f1adcfd4bfa80c87cec9151ce9da1e330f
AlcatrazEscapee <alex@molleroneill.com>
2021-12-18 14:53:15 -0500

    Revery day17 to the canonical implementation as described in the paper as the optimization of collecting (n, vy) values didn't turn out to be that effective in practice, and it had a similar worst case theoretical complexity to the simple implementation.

--- 2021/src/day17.py
+++ 2021/src/day17.py
@@ -1,37 +1,30 @@
 from utils import get_input, ints
 
 from math import sqrt, floor, ceil
-from collections import defaultdict
 
 
 def main(text: str):
     min_x, max_x, min_y, max_y = ints(text)
-
-    n2v = defaultdict(set)
+    trajectories = set()
 
     for yt in range(min_y, 1 + max_y):
         for n in range(1, 1 + 2 * abs(yt)):
-            if (2 * yt) % n == 0 and (v0 := 2 * yt - n + n * n) % (2 * n) == 0:
+            if (v0 := 2 * yt - n + n * n) % (2 * n) == 0:
                 v = v0 // (2 * n)
-                n2v[n].add(v)
-
-    trajectories = set()
-    for xt in range(min_x, 1 + max_x):
-        k = sqrt(2 * xt)
-        for u in range(floor(k) - 1, 1 + ceil(k)):
-            if u * (u + 1) == 2 * xt:
-                for n, vs in n2v.items():
-                    if u <= n:
-                        for v in vs:
+                for xt in range(min_x, 1 + max_x):
+                    # Case: u <= n
+                    k = sqrt(2 * xt)
+                    for u in range(floor(k) - 1, 1 + ceil(k)):
+                        if u * (u + 1) == 2 * xt:
+                            if u <= n:
+                                trajectories.add((u, v))
+                    # Case: u > n
+                    if (u0 := 2 * xt - n + n * n) % (2 * n) == 0:
+                        u = u0 // (2 * n)
+                        if u > n:
                             trajectories.add((u, v))
-        for n, vs in n2v.items():
-            if (2 * xt) % n == 0 and (u0 := 2 * xt - n + n * n) % (2 * n) == 0:
-                u = u0 // (2 * n)
-                if u > n:
-                    for v in vs:
-                        trajectories.add((u, v))
 
-    print('Part 1:',  max(v * (v + 1) // 2 for _, v in trajectories if v > 0))
+    print('Part 1:', max(v * (v + 1) // 2 for _, v in trajectories if v > 0))
     print('Part 2:', len(trajectories))
 
 
