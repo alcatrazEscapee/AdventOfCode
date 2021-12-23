7ee54ae311c2895c02566431fd688b3fb282cc81
AlcatrazEscapee <alex@molleroneill.com>
2021-12-23 13:20:50 -0700

    Day 6, skipped day 5 due to not wanting to bother with md5 for libraries. Fairly straightforward due to the `transpose` function from day 3 and then the character frequency sorting from day 4.

---
+++ 2016/src/day06.hs
@@ -0,0 +1,19 @@
+import Data.Ord
+
+import qualified Data.List as List
+import qualified Data.Map as Map
+
+main :: IO ()
+main = do
+    inp <- getContents
+    let freq = map characterFrequencies . transpose . lines $ inp
+    putStrLn $ "Part 1: " ++ (map last freq)
+    putStrLn $ "Part 2: " ++ (map head freq)
+
+characterFrequencies :: String -> [Char] 
+characterFrequencies = map fst . List.sortBy (comparing snd) . Map.toList . foldl account Map.empty
+    where account counter c = Map.insertWith (+) c 1 counter
+
+transpose :: [[a]] -> [[a]]
+transpose ([]:_) = []
+transpose x = (map head x) : transpose (map tail x)
