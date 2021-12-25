b625299a3670b4248f9ee32634c8374ab673afb1
AlcatrazEscapee <alex@molleroneill.com>
2021-12-24 23:15:48 -0700

    Some minor optimizations for day 25, cleanup main.py's import hack, and add an actual readme.

--- 2021/main.py
+++ 2021/main.py
@@ -8,34 +8,32 @@ from io import StringIO
 
 from utils import get_input
 
-try:
-    from day01 import day01
-    from day02 import day02
-    from day03 import day03
-    from day04 import day04
-    from day05 import day05
-    from day06 import day06
-    from day07 import day07
-    from day08 import day08
-    from day09 import day09
-    from day10 import day10
-    from day11 import day11
-    from day12 import day12
-    from day13 import day13
-    from day14 import day14
-    from day15 import day15
-    from day16 import day16
-    from day17 import day17
-    from day18 import day18
-    from day19 import day19
-    from day20 import day20
-    from day21 import day21
-    from day22 import day22
-    from day23 import day23
-    from day24 import day24
-    from day25 import day25
-except:
-    pass
+from day01 import day01
+from day02 import day02
+from day03 import day03
+from day04 import day04
+from day05 import day05
+from day06 import day06
+from day07 import day07
+from day08 import day08
+from day09 import day09
+from day10 import day10
+from day11 import day11
+from day12 import day12
+from day13 import day13
+from day14 import day14
+from day15 import day15
+from day16 import day16
+from day17 import day17
+from day18 import day18
+from day19 import day19
+from day20 import day20
+from day21 import day21
+from day22 import day22
+from day23 import day23
+from day24 import day24
+from day25 import day25
+
 
 PuzzleOutput = Tuple[Optional[str], Optional[str]]
 
--- 2021/src/day25.py
+++ 2021/src/day25.py
@@ -1,4 +1,5 @@
 # Day 25: Sea Cucumber
+
 from utils import FiniteGrid, get_input
 
 
@@ -11,6 +12,7 @@ def main(text: str):
         next_grid = FiniteGrid.of_empty(grid.width, grid.height, '.')
         movement = False
 
+        # Move the east herd first. Check against the existing open spaces
         for x, y in grid.locations():
             if grid[x, y] == '>':
                 x0 = (x + 1) % grid.width
@@ -19,22 +21,17 @@ def main(text: str):
                     next_grid[x0, y] = '>'
                 else:
                     next_grid[x, y] = '>'
-            elif grid[x, y] == 'v':
-                next_grid[x, y] = 'v'
 
-        grid = next_grid
-        next_grid = FiniteGrid.of_empty(grid.width, grid.height, '.')
-
+        # Move the south herd second. The herd locations are in grid, but the east locations are in next_grid
+        # An 'empty' spot, has grid[x,y] != 'v', and also that next_grid[x, y] != '>'
         for x, y in grid.locations():
             if grid[x, y] == 'v':
                 y0 = (y + 1) % grid.height
-                if grid[x, y0] == '.':
+                if grid[x, y0] != 'v' and next_grid[x, y0] != '>':
                     next_grid[x, y0] = 'v'
                     movement = True
                 else:
                     next_grid[x, y] = 'v'
-            elif grid[x, y] == '>':
-                next_grid[x, y] = '>'
 
         grid = next_grid
         step += 1
