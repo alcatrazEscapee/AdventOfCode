9909404e60934af50dad61ba4691c2e2a4f71e01
AlcatrazEscapee <alex@molleroneill.com>
2022-12-15 01:58:57 -0500

    Cleaned up Day 15, with clean, nice, fast, implementations for Part 1 and Part 2. Both run in a few ms, and added detailed comments explaining the geometry.

--- 2022/src/day15.py
+++ 2022/src/day15.py
@@ -1,112 +1,114 @@
-from utils_all import *
+from utils import get_input, ints
+from typing import NamedTuple, Optional, Iterator
 
-# READ THE PROBLEM; UNDERSTAND THE PROBLEM; IMPLEMENT THE SOLUTION
-# Remember: ints() for parsing, Counter, Point2/3, Grids (Finite/Infinite), A*, DP (lru_cache)
 
 def main(text: str):
-    lines = text.split('\n')
-
-    # p1 5733732
-    # p1 4985193
-
-    data = []
-    for line in lines:
-        sx, sy, bx, by = ints(line)
-        dist = norm1((sx - bx, sy - by))
-        data.append([sx, sy, bx, by, dist])
-
-
-    MAX = 2000000#20
-
-    possible = set()
-    all_possibles = []
-    for sx, sy, bx, by, dist in data:
-        this = set()
-        #print(sx, sy, bx, by, dist)
-        for dx in range(-dist - 1, 2 + dist):
-            P = (sx + dx, sy + abs(dx) - abs(dist) - 1)
-            Q = (sx + dx, sy - (abs(dx) - abs(dist) - 1))
-            #print('doing', dx, P, Q)
-            possible.add(P)
-            possible.add(Q)
-        print('results', len(possible))
-        all_possibles.append(this)
-    #possible = functools.reduce(lambda x, y: x | y, all_possibles)
-    possible = [(x, y) for x, y in possible if x <= MAX and y <= MAX and x >= 0 and y >= 0]
-    #print((14, 11) in possible, possible)
-    print('in total', len(possible))
-    for px, py in possible:
-        can = True
-        for sx, sy, bx, by, dist in data:
-            if norm1((sx - px, sy - py)) <= dist:
-                can = False
-                break
-        if can:
-            print('FOUND THE ANSWER', px, py)
-
-
-    return
-    for x in range(0, 20):
-        for y in range(0, 20):
-            can = True
-            dists = set()
-            for sx, sy, bx, by, dist in data:
-                if abs(x - sx) + abs(y - sy) <= abs(sx - bx) + abs(sy - by):
-                    t = abs(x - sx) + abs(y - sy)
-                    can = False
-                    if t in dists:
-                        can = False
-                    dists.add(t)
-            if can:
-
-                print('can slow', x, y)
-
-    return
-    ty = 2000000
     ranges = []
-    for sx, sy, bx, by, dist in data:
-        dy = abs(sy - ty)
-        dx = dist - dy
+    areas = []
+    width = 2000000
+    for line in text.split('\n'):
+        sx, sy, bx, by = ints(line)
+        r = abs(sx - bx) + abs(sy - by)  # Radius of the diamond
+
+        dy = abs(sy - width)
+        dx = r - dy
         if dx > 0:
             ranges.append((sx - dx, sx + dx))
 
-    ranges = sorted(ranges, key=lambda x: x[0])
-    print('ranges', ranges)
+        x0, y0 = sx, sy - r  # Bottom -> Bottom left
+        x1, y1 = sx, sy + r  # Top -> Top Right
 
-    ranges2 = list(ranges)
-    joined = []
-    while len(ranges2) >= 2:
-        a, b, *rest = ranges2
-        c = merge_range(a, b, joined)
-        print('merged', c)
-        rest.insert(0, c)
-        ranges2 = rest
-        print(ranges2)
+        # Coordinates of the new transformed square area
+        x0, y0 = transform(x0, y0)
+        x1, y1 = transform(x1, y1)
 
-    print('final', joined, ranges2)
+        areas.append(Rect(x0, x1, y0, y1))
 
-    ret = sum(map(lambda x: x[1] - x[0], ranges2))
+    # Part 1, we implement by finding the illegal ranges, and then joining ranges together to obtain a set of disjoint ranges
 
-def merge_range(a, b, joined):
-    a0, a1 = a
-    b0, b1 = b
-    #print(a, b, '->', joined)
-    assert a0 <= b0
-    if b1 <= a1:
-        return a0, a1  # contained in
-    if b0 <= a1:
-    #    print('merge')
-        return a0, b1  # merge
-    if b0 > a1:
-        # disjoint
-        joined.append((a0, a1))
-        return b0, b1
-    #print('wut', a, b)
+    ranges = sorted(ranges, reverse=True)
+    disjoint_ranges = []
+    while len(ranges) >= 2:
+        # Treating the ranges like a stack, merge them two at a time.
+        # If we have a result which cannot be merged, then discard the newly found disjoint range
+        a = a0, a1 = ranges.pop(-1)
+        b = b0, b1 = ranges.pop(-1)
+
+        if a1 < b0:  # Disjoint ranges
+            disjoint_ranges.append(a)
+            ranges.append(b)
+        elif b1 <= a1:  # b is a subset of a, so discard b
+            ranges.append(a)
+        else:  # b1 > a1, so merge the two ranges
+            ranges.append((a0, b1))
+
+    print('Part 1:', sum(map(lambda v: v[1] - v[0], ranges)))
+
+    # For part 2, we implement a coordinate subdivision method
+    # We use `transform()` to skew the coordinates, such that each sensor's manhattan distance area is actually a square
+    # From there we can reason about square areas, subdividing, and intersecting them and only retaining the disjoint square regions in which no sensor is covering
+    # Since there are not many sensors, this eliminates areas very quickly (total runtime ~2ms)
+
+    # For the 'initially valid' state, we just pick a square (in original coordinates) diamond, which is 'large enough'
+    # It needs to contain original square, which means we pick a much larger diamond.
+    # Excess area is not a problem, since we identify it's contigous and won't accidentally find it as the valid solution
+    x0, y0 = transform(0, -3 * width)
+    x1, y1 = transform(0, 3 * width)
+
+    remaining = [Rect(x0, x1, y0, y1)]  # All positions that are still valid
+    for area in areas:
+        new_remaining = []
+        for rem in remaining:
+            intersect = rem.intersect(area)  # Found an intersection, so subdivide and retain only disjoint parts not intersecting `area`
+            if intersect is not None:
+                for part in rem.decompose(intersect):
+                    new_remaining.append(part)
+            else:
+                new_remaining.append(rem)
+        remaining = new_remaining
+
+    # At this point according to the problem, we'll only have a single point remaining that is of area = 1
+    # Simply find it and show the result.
+    x0, y0 = next(untransform(rem.x0, rem.y0) for rem in remaining if rem.area() == 1)
+    print('Part 2:', x0 * 4000000 + y0)
+
+def transform(x: int, y: int):
+    return x + y, y - x
+
+def untransform(x: int, y: int):
+    return (x - y) // 2, (x + y) // 2
 
 
+class Rect(NamedTuple):
+    """ A 2-dimensional box defined by [x0, x1] x [y0, y1]. Borrowed form 2021 Day 22 """
+    x0: int
+    x1: int
+    y0: int
+    y1: int
 
+    def intersect(self, other: 'Rect') -> Optional['Rect']:
+        """ Compute the intersection of this box with another, if it exists. """
+        if not (self.x1 < other.x0 or self.x0 > other.x1 or
+                self.y1 < other.y0 or self.y0 > other.y1):
+            return Rect(
+                max(self.x0, other.x0), min(self.x1, other.x1),
+                max(self.y0, other.y0), min(self.y1, other.y1)
+            )
 
+    def decompose(self, other: 'Rect') -> Iterator['Rect']:
+        """ Where other is a strict subset of this rect, return a set of disjoint boxes which partition a - b """
+        a, b = self, other
+        for rect in (
+            Rect(a.x0, b.x0 - 1, a.y0, a.y1),
+            Rect(b.x1 + 1, a.x1, a.y0, a.y1),
+            Rect(b.x0, b.x1, a.y0, b.y0 - 1),
+            Rect(b.x0, b.x1, b.y1 + 1, a.y1),
+        ):
+            if rect.area() > 0:
+                yield rect
 
+    def area(self) -> int:
+        return max(0, 1 + self.x1 - self.x0) * max(0, 1 + self.y1 - self.y0)
 
 
 if __name__ == '__main__':
