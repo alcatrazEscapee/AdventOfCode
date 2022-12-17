d37af482229cf9b2ab5a3aa26e11bc2b3bb6f020
AlcatrazEscapee <alex@molleroneill.com>
2022-12-17 13:45:08 -0500

    Day 17, cleaned up. Rank was 112 / 401, I knew that I needed to detect a cycle but was struggling figuring out exactly how to do so. This also has a customizable 'history' value, which is used for efficiency and cycle detection. It's a heuristic, but it seems to work and is obvious when it's too low.

--- 2022/src/day17.py
+++ 2022/src/day17.py
@@ -1,137 +1,98 @@
+# Day 17: Pyroclastic Flow
+# Rank: 112 / 401
+
 from utils import get_input
-from math import lcm
+from typing import Tuple, Set
 
 
-def main(text: str):
-    lines = text.split('\n')
+ROCKS: Tuple[Set[Tuple[int, int]], ...] = (
+    {(0, 0), (1, 0), (2, 0), (3, 0)},  # Horizontal
+    {(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)},  # Plus
+    {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)},  # Backwards L
+    {(0, 0), (0, 1), (0, 2), (0, 3)},  # Vertical
+    {(0, 0), (0, 1), (1, 0), (1, 1)},  # Square
+)
+
+# A modifier for the amount of history to keep in the tetris grid
+# This both improves the speed (as less history means less positions to check), and ease of detecting cycles for part 2
+# On my input at least, any history value < 50 will cause pieces to drop off the board (which the solution below will detect and throw an exception for)
+HISTORY = 50
 
 
-    #vals = [719, 1106 - 719, 2811 - 1106, 2860 - 2811]
-    #print(sum(vals) * 250000000000)
+def main(jets: str):
 
-
-    ROCKS = [
-        {(0, 0), (1, 0), (2, 0), (3, 0)},  # line
-        {(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)},  # plus
-        {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)},  # L backwards
-        {(0, 0), (0, 1), (0, 2), (0, 3)},
-        {(0, 0), (0, 1), (1, 0), (1, 1)}
-    ]
-
-    last = 0
-
-    MOVES = text
-    #MOVES = '>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>'
-
-    inverval = lcm(len(MOVES), len(ROCKS))
-
-    seen = set()
-    seen2 = set()
-
-    time = 1000000000000 - 120
-    p1 = False
-
-    start_cyc = 0
-
-    jet_tick = 0
-    rock_tick = 0
+    jet_tick = rock_tick = 0
     floor = {(x, 0) for x in range(7)}
-    num_rocks = 0
-    last_delta = 0
-    last_num_rocks = 0
-    bonus_y = 0
-    p2 = False
+    total_rocks = 0
+
+    # Phase of the iteration
+    # 'part1' = Running up until rock 2022, for part 1
+    # 'wait' = Waiting for a cycle to be detected
+    # 'part2' = Filling in the last remaining rows until part 2 is done
+    phase = 'part1'
+
+    seen = dict()  # Prior states, mapped to the (max y, total rocks)
+    skip_y: int = 0  # A y value to add, counting for skipped iterations due to the cycle
+
     while True:
+        max_y = max(y for _, y in floor)
 
-        if num_rocks % 100 == 0:
-            print(num_rocks)
-        max_y_in_floor = max(y for _, y in floor)
-        #print(max_y_in_floor)
+        if total_rocks == 2022:  # Phase 'part1'
+            print('Part 1:', max_y)
+            phase = 'wait'  # Move onto cycle detection
 
-        top_floor = frozenset((x, y - max_y_in_floor) for x, y in floor if y >= max_y_in_floor - 70)
+        if total_rocks == 1000000000000:  # Phase 'part2'
+            print('Part 2:', max_y + skip_y)
+            return  # And exit
 
+        if phase == 'wait':
+            top_floor = frozenset((x, y - max_y) for x, y in floor if y >= max_y - HISTORY)
+            state = top_floor, rock_tick, jet_tick
+            if state in seen:
+                prior_max_y, prior_total_rocks = seen[state]
 
+                # Skip ahead by the known interval
+                cycle_length = total_rocks - prior_total_rocks
+                cycle_count = (1000000000000 - total_rocks - 1) // cycle_length
 
-        st = ((top_floor, rock_tick, jet_tick))
+                total_rocks += cycle_count * cycle_length
+                skip_y += (max_y - prior_max_y) * cycle_count
+            else:
+                seen[state] = max_y, total_rocks
 
-        if st in seen2 and not p2:
-            print('recurse again at', num_rocks)
-            old_n_rocks, old_y = start_cyc
-            print('from', *start_cyc, 'to', num_rocks, max_y_in_floor)
-            print('cycle len', cyc_len := (num_rocks - old_n_rocks))
-            print(repeats := (1000000000000 - num_rocks) // cyc_len)
-            num_rocks += repeats * cyc_len
-            bonus_y = (max_y_in_floor - old_y) * repeats
-            p2 = True
-            print('bonus y', bonus_y, 'and now', num_rocks)
+        # Drop any from the floor older than our history
+        # This is not strictly necessary, but it improves performance at the cost of having an unsound solution (as a flat history value could produce invalid results)
+        floor = {(x, y) for x, y in floor if y > max_y - HISTORY}
 
-        if st in seen:
-            if not p1:
-                print('recurse! at', num_rocks)
-                start_cyc = num_rocks, max_y_in_floor
-            p1 = True
-            seen2.add(st)
-        seen.add(st)
-
-        floor = {(x, y) for x, y in floor if y > max_y_in_floor - 100}
-
-        if num_rocks % inverval == 0:
-            print(num_rocks, num_rocks - last_num_rocks, max_y_in_floor, max_y_in_floor - last)
-            last = max_y_in_floor
-            last_num_rocks = num_rocks
-
-        #if sum((x, max_y_in_floor) in floor for x in range(7)) == 6:
-            #yield jet_tick, rock_tick
-            #print('yes at', num_rocks)
-            #print('max y', max_y_in_floor)
-
-        rock = {(x + 2, max_y_in_floor + y + 4) for x, y in ROCKS[rock_tick]}
+        # Advance the rock
+        rock = {(x + 2, max_y + y + 4) for x, y in ROCKS[rock_tick]}
         rock_tick = (rock_tick + 1) % len(ROCKS)
 
         while True:
-            jet = MOVES[jet_tick]
-            jet_tick = (jet_tick + 1) % len(MOVES)
+            jet = jets[jet_tick]
+            jet_tick = (jet_tick + 1) % len(jets)
 
-            #print(rock)
-
-            #print('===')
-            #print(InfiniteGrid.of_points({(x, -y) for x, y in floor | rock}))
-            #print('===')
-
-
-            # BLOCKED 0 1 2 3 4 5 6 BLOCKED
-
+            # Move horizontally, if possible
             if jet == '<':
                 test_rock = {(x - 1, y) for x, y in rock}
-                if any(r in floor for r in test_rock) or any(x < 0 for x, _ in test_rock):  # blocked
-                    pass
-                else:
+                if all((x, y) not in floor and x >= 0 for x, y in test_rock):
                     rock = test_rock
             elif jet == '>':
                 test_rock = {(x + 1, y) for x, y in rock}
-                if any(r in floor for r in test_rock) or any(x > 6 for x, _ in test_rock):  # blocked
-                    pass
-                else:
+                if all((x, y) not in floor and x < 7 for x, y in test_rock):
                     rock = test_rock
 
-            # move down
+            # Move down, and stop if we reach solid ground
             test_rock = {(x, y - 1) for x, y in rock}
             if any(r in floor for r in test_rock):
-                # stop
-                #print('is down')
                 floor |= rock
-                num_rocks += 1
-                #yield jet_tick, rock_tick, max(y for _, y in floor)
-                if num_rocks == 1000000000000:
-                    print(max(y for _, y in floor) + bonus_y)
-                    return
-                #jet_tick = (jet_tick + 1) % len(MOVES)
-                break  # new rock
+                total_rocks += 1
+                break
             else:
                 rock = test_rock
-        #if num_rocks > 4:
-        #    return
 
+            if any(y < 0 for _, y in rock):
+                raise ValueError('HISTORY = %d is too low, a piece managed to drop through the entire board!' % HISTORY)
 
 
 if __name__ == '__main__':
