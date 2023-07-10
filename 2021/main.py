d0fd8b949f2ae29a92cd5413bab575ec7072d163
AlcatrazEscapee <alex@molleroneill.com>
2023-07-10 08:10:02 -0400

    Refactor test runner to rely on answers in `/inputs/answers.txt`

--- 2021/main.py
+++ 2021/main.py
@@ -1,80 +1,76 @@
-# Entrypoint for running all, or individual puzzles
-# Times each puzzle's execution
+# Runs each day, checks against expected answer, and reports runtime. 
+#
+# Puzzle inputs go in a folder /inputs/day##.txt
+# Example inputs (if present) go in a folder /inputs/day##_examples.txt
+# Each example should be separated by '\n\n=====\n\n'. Examples are optional
+# Answers go in a file /inputs/answers.txt in csv format, with no headers
+#
+# /inputs/answers.txt
+# 1234,ABCD,abcd,...
+#
+# Run with `python main.py` in the root directory.
 
-from typing import NamedTuple, Tuple, Optional, Any
-from contextlib import redirect_stdout
-from time import perf_counter_ns
+import sys
+import csv
+import unittest
+
 from io import StringIO
-
+from time import time_ns
 from utils import get_input
+from contextlib import redirect_stdout
+
 from src import day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
 
+DAYS = day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+TIMES = {}
 
-PuzzleOutput = Tuple[Optional[str], Optional[str]]
-Days = day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+class AoCTest(unittest.TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        print('\n\n=== Runtime Summary ===')
+        for day, t in TIMES.items():
+            print('Day %02d : %d ms' % (day, t))
 
-class ExampleInput(NamedTuple):
-    example: int
 
+def make_test_method(name: str, example: int, day: int, module, part1_expected: str, part2_expected: str):
+    def apply(self):
+        if example == -1:
+            inp = get_input(day, './inputs/day%02d.txt')
+        else:
+            inp = get_input(day, './inputs/day%02d_examples.txt').split('\n\n=====\n\n')[example]
+        start = time_ns()
+        with StringIO() as out, redirect_stdout(out):
+            module.main(inp)
+            output = out.getvalue()
+        end = time_ns()
 
-def main():
-    benchmark_all()
+        part1 = part2 = 'None'
+        for line in output.split('\n'):
+            if line.startswith('Part 1: '):
+                part1 = line[8:]
+            elif line.startswith('Part 2: '):
+                part2 = line[8:]
 
-def benchmark_all():
+        self.assertEqual(part1_expected, part1)
+        self.assertEqual(part2_expected, part2)
+        TIMES[day] = (end - start) // 1_000_000
+    
+    apply.__name__ = name
+    setattr(AoCTest, apply.__name__, apply)
+
+def setup():
+    with open('./inputs/answers.txt', 'r', encoding='utf-8') as f:
+        answers = list(csv.reader(f))
+
     for day in range(1, 1 + 25):
-        benchmark_day(day)
-
-def benchmark_day(day: int):
-    try:
-        inp = resolve_input(day)
-        tick = perf_counter_ns()
-        exec_day(day, inp)
-        tock = perf_counter_ns()
-        print('Day %02d:' % day, format_ns(tock - tick))
-    except Exception as e:
-        print('Day %02d:' % day, e)
-
-def run_day(day: int, inp: Any = None) -> PuzzleOutput:
-    inp = resolve_input(day, inp)
-    out = exec_day(day, inp)
-    return decode_output(out)
-
-def exec_day(day: int, inp: str) -> str:
-    with StringIO() as out, redirect_stdout(out):
-        Days[day - 1].main(inp)
-        return out.getvalue()
-
-# Inputs
-
-def resolve_input(day: int, inp: Any = None) -> str:
-    if inp is None:
-        return get_input(day, './inputs/day%02d.txt')
-    elif isinstance(inp, ExampleInput):
-        return get_input(day, './inputs/day%02d_examples.txt').split('\n\n=====\n\n')[inp.example - 1]
-    return inp
-
-def example(index: int) -> ExampleInput:
-    return ExampleInput(index)
-
-def decode_output(output: str) -> PuzzleOutput:
-    part1 = part2 = None
-    for line in output.split('\n'):
-        if line.startswith('Part 1: '):
-            part1 = line[8:]
-        elif line.startswith('Part 2: '):
-            part2 = line[8:]
-    return part1, part2
-
-def format_ns(delta: int) -> str:
-    if delta < 10_000:
-        return '%5d ns' % delta
-    elif delta < 10_000_000:
-        return '%5d us' % int(delta * 0.001)
-    elif delta < 10_000_000_000:
-        return '%5d ms' % int(delta * 0.000_001)
-    else:
-        return '%5d  s' % int(delta * 0.000_000_001)
+        part1, part2, *examples = answers[day - 1]
+        make_test_method('test day %02d' % day, -1, day, DAYS[day - 1], part1, part2)
+        
+        for example in range(len(examples) // 2):
+            make_test_method('test day %02d example %d' % (day, example), example, day, DAYS[day - 1], examples[2 * example], examples[2 * example + 1])
 
 
 if __name__ == '__main__':
-    main()
+    setup()
+    sys.argv = sys.argv[:1]
+    unittest.main()
