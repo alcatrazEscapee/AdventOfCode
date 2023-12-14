0189acfc5dae0f9d1e11022e92c7756c2f36849a
AlcatrazEscapee <alex@molleroneill.com>
2023-12-14 08:35:41 -0500

    Clean up some unhandled cases and lint warnings

--- 2023/src/day05.hs
+++ 2023/src/day05.hs
@@ -4,20 +4,17 @@ import qualified Data.Maybe as Maybe
 main :: IO ()
 main = do
     inp <- getContents
-    let (seeds, maps) = parse . lines $ inp
+    let (seeds, maps) = parse inp
     putStrLn $ "Part 1: " ++ (part1 maps seeds)
     putStrLn $ "Part 2: " ++ (part2 maps seeds)
 
 
-parse :: [String] -> ([Int], [[[Int]]])
-parse (x:xs) = (seeds, maps)
-    where seeds = map read . drop 1 . words $ x
-          maps = map (map (map read . words) . drop 1) . splitEmpty $ xs
-
-          splitEmpty :: [String] -> [[String]]
-          splitEmpty []     = []
-          splitEmpty (_:xs) = a : splitEmpty b
-               where (a, b) = break (== []) xs
+parse :: String -> ([Int], [[[Int]]])
+parse inp = (seeds, maps)
+    where (x:_:xs) = lines inp
+          
+          seeds = map read . drop 1 . words $ x
+          maps  = map (map (map read . words) . drop 1) . splitEmpty $ xs
 
 
 part1 :: [[[Int]]] -> [Int] -> String
@@ -27,6 +24,7 @@ part1 maps = show . minimum . map (foldl1 (flip (.)) . map apply $ maps)
           apply ((dest:src:len:[]):rs) x = if src <= x && x < src + len
                                            then x - src + dest
                                            else apply rs x
+          apply _                      _ = error "Invalid range"
 
 
 part2 :: [[[Int]]] -> [Int] -> String
@@ -44,7 +42,15 @@ part2 maps = show . minimum . map fst . concatMap (foldl1 (\f g -> concatMap g .
                       -- They don't intersect this range, and so they need to be called with `apply` recursively
                       mapL = if x        < intL then Just (x, intL - x)         else Nothing
                       mapR = if (x + xl) > intR then Just (intR, x + xl - intR) else Nothing
+          apply _                      _       = error "Invalid range"
 
-          take2 :: [a] -> [(a, a)]
-          take2 []       = []
-          take2 (a:b:xs) = (a, b) : take2 xs
+
+splitEmpty :: [String] -> [[String]]
+splitEmpty xs = case break (== []) xs of
+                        (a, _:bs) -> a : splitEmpty bs
+                        (a, []) -> [a]
+
+take2 :: [a] -> [(a, a)]
+take2 []       = []
+take2 (a:b:xs) = (a, b) : take2 xs
+take2 _        = error "Not a multiple of 2"
